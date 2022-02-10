7623b2b7b5a2845710d13301420c1bd2653ec122
Benjamin Hendel <bhendel@veracode.com>
2022-02-09 19:04:30 -0500

    Implemented 'use' command.

--- cryptosploit.py
+++ cryptosploit.py
@@ -4,10 +4,15 @@ import importlib
 from modules import *
 from tabulate import tabulate
 
-def printBanner(version, onlineAttacks, offlineAttacks):
+version = "1.0"
+modules = []
+moduleClasses = None
+
+currentModule = None
+
+def printBanner():
     versionLine = "{0:<28}".format("Cryptosploit v" + version)
-    onlineAttackLine = "{0:<28}".format(str(onlineAttacks) + " online attacks")
-    offlineAttackLine = "{0:<28}".format(str(offlineAttacks) + " offline attacks")
+    modulesLine = "{0:<28}".format(str(len(modules)) + " modules")
     print("""\
 
  █▀▀ █▀█ █▄█ █▀█ ▀█▀ █▀█ █▀ █▀█ █░░ █▀█ █ ▀█▀
@@ -17,9 +22,9 @@ def printBanner(version, onlineAttacks, offlineAttacks):
                                                ████████                    
                                              ██        ██                  
   [ """ + versionLine + """ ]         ██            ██████████████████
-  [ """ + onlineAttackLine + """ ]         ██  ████                      ██
-  [ """ + offlineAttackLine + """ ]         ██  ████      ░░░░██░░██░░██░░██
-  [ github.com/cryptosploit      ]         ██░░        ░░████  ██  ██  ██  
+  [ """ + modulesLine + """ ]         ██  ████                      ██
+                                           ██  ████      ░░░░██░░██░░██░░██
+                                           ██░░        ░░████  ██  ██  ██  
                                              ██░░░░░░░░██                  
                                                ████████                    
 
@@ -29,19 +34,37 @@ def printBanner(version, onlineAttacks, offlineAttacks):
 
 def printHelp():
     print("\n\n> Help here. <\n\n")
-       
-def listModules():
-    modules = []
+
+def getModuleList():
+    global moduleClasses
     moduleClasses = AbstractModule.__subclasses__()
     for c in moduleClasses:
         modules.append([c.name, c.description])
+        
+def listModules(): 
     print('\n', tabulate(modules, headers=['Name', 'Description']), '\n')
 
+def useModule(module):
+    global currentModule;
+    moduleClass = next((c for c in moduleClasses if c.name == module), None)
+    if moduleClass == None:
+        print("No module named '{}' found.\n".format(module))
+    else:
+        currentModule = moduleClass
+
+def showOptions():
+    pass
+    
 def handleCommands():
     while True:
         # TODO: add module name to input prompt if finite state machine allows it
         # csp(someattack)>
-        cmd, *args = shlex.split(input('csp> '))
+        inputPrompt = None
+        if currentModule != None:
+            inputPrompt = 'csp({})> '.format(currentModule.name)
+        else:
+            inputPrompt = 'csp> '
+        cmd, *args = shlex.split(input(inputPrompt))
 
         # TODO: implement state machine to track which commands are currently available
         # https://refactoring.guru/design-patterns/state
@@ -49,12 +72,19 @@ def handleCommands():
             printHelp()
         elif cmd == 'listmods':
             listModules()
+        elif cmd == 'use':
+            if len(args) == 0:
+                printHelp()
+            else:
+                useModule(args[0])
+        elif cmd == 'options':
+            showOptions()
         else:
             print("Unknown command '{cmd}'. Type 'help' for help.\n\n".format(cmd=cmd))
     
 def main():
-    # TODO: get module counts here, should probably preload info for getModules()
-    printBanner("1.0", 10, 12)
+    getModuleList()
+    printBanner()
     handleCommands()
 
 if __name__ == "__main__":
