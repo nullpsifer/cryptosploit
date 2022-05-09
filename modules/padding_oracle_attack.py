338e1ac639fc018c1115abde9a75cb2f2d56d829
Matt Cheung <mgcheung@gmail.com>
2022-05-09 18:40:48 -0400

    Remove oracle options from padding oracle attack module

--- modules/padding_oracle_attack.py
+++ modules/padding_oracle_attack.py
@@ -8,41 +8,13 @@ class PaddingOracleModule(AbstractModule):
 
     description = 'Runs a CBC Padding Oracle Attack'
 
-    arguments = [ModuleArgumentDescription('ciphertext','Ciphertext to decrypt',True),
-                 ModuleArgumentDescription('url','URL to attack',False),
-                 ModuleArgumentDescription('verb','Whether this is through a GET or POST request',False),
-                 ModuleArgumentDescription('cipherparam','Parameter for ciphertext',False),
-                 ModuleArgumentDescription('params','Any other params and values in JSON format',False,defaultValue='{}'),
-                 ModuleArgumentDescription('goodpad','Status code for good pad',False),
-                 ModuleArgumentDescription('badpad', 'Status code for bad pad',False)]
+    arguments = [ModuleArgumentDescription('ciphertext','Ciphertext to decrypt',True)]
                  
     oracle = None # probably not good
-    oracleRequired = False
+    oracleRequired = True
 
     def execute(self):
         ciphertext = self.get_argument_value('ciphertext')
-        url = self.get_argument_value('url')
-        verb = self.get_argument_value('verb')
-        cipherparam = self.get_argument_value('cipherparam')
-        paramstring = self.get_argument_value('params')
-        #params = loads()
-        params = {}
-        goodpad = int(self.get_argument_value('goodpad'))
-        badpad = int(self.get_argument_value('badpad'))
-
-        def makeoracle():
-            if verb == 'GET':
-                def oracle(ctext):
-                    params[cipherparam] = ctext
-                    r = requests.get(url,params)
-                    return r.status_code != badpad
-                return oracle
-            elif verb == 'POST':
-                def oracle(ctext):
-                    params[cipherparam] = ctext
-                    r = requests.post(url,params=params)
-                    return r.status_code != badpad
-                return oracle
 
         poa = PaddingOracleAttack(ciphertext,makeoracle())
         plaintext = poa.runattack()
