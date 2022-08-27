780bc9224f3623fa76a72e70dc04303597ec95f7
Matt Cheung <mgcheung@gmail.com>
2022-08-26 20:40:42 -0400

    Start of RSA signature fault attack. Applies Sunzi's theorem (Chinese Remainder Theorem). Planning to add the ability to detect if a single bit of the private exponent is flipped and extracting that bit.

---
+++ modules/rsasignaturefault.py
@@ -0,0 +1,51 @@
+from utils.pkcs15 import PKCS15
+from modules.abstract_module import *
+from json import loads
+from gmpy2 import gcd
+
+class RSASignatureFault(AbstractModule):
+    name = 'rsa_signature_fault'
+
+    description = 'This module queries a signature oracle until it gets a repeated r value and then computes the private key'
+    arguments = [ModuleArgumentDescription('public_key','Public Signing Key', True)]
+    oracle = None
+    oracleRequired = True
+
+    def execute(self):
+        hashfunctions = ['sha256','sha1','md5','sha384','sha512']
+        keyRecovered = False
+        if isinstance(self.get_argument_value('public_key'),str):
+            public_key = loads(self.get_argument_value('public_key'))
+        else:
+            public_key = self.get_argument_value('public_key')
+        e = public_key['e']
+        n = public_key['n']
+        privatekey = {}
+        bytelength = n.bit_length()//8
+        mypkcs = PKCS15(bytelength)
+        while not keyRecovered:
+            oracle = self.oracle.makeoracle()
+            data = oracle()
+            m = data['m']
+            s = data['s']
+            checkvalue = pow(s,e,n)
+            check = checkvalue.to_bytes(bytelength,'big')
+            if check[:2] != b'\x00\x01':
+                for hashfunction in hashfunctions:
+                    potentiallycorrectvalue = mypkcs.signaturePad(m.encode('utf-8'),hashfunction)
+                    cd = int(gcd(checkvalue-potentiallycorrectvalue,n))
+                    if 1 < cd < n:
+                        p = cd
+                        q = n//p
+                        if q<p:
+                            q,p=p,q
+                        privatekey['p'] = p
+                        privatekey['q'] = q
+                        privatekey['n'] = n
+                        privatekey['d'] = pow(e,-1,(p-1)*(q-1))
+                        privatekey['dp'] = privatekey['d'] % (p-1)
+                        privatekey['dq'] = privatekey['d'] % (q - 1)
+                        keyRecovered = True
+                        break
+        print(f'Private key: {privatekey}')
+        return privatekey
