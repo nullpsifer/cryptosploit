a8f9f83140d93bb52fc3ca3afdd0feaafc04393b
Matt Cheung <mgcheung@gmail.com>
2022-10-19 14:23:30 -0400

    Repeated XOR module

---
+++ modules/repeatedxor.py
@@ -0,0 +1,19 @@
+from utils.binutils import RepeatedKeyXOR
+from modules.abstract_module import *
+
+class RepeatedXOR(AbstractModule):
+    name = 'repeated_xor'
+
+    description = 'This module applies a repeated xor key to an input'
+    arguments = [ModuleArgumentDescription('key','XOR key', True),
+                 ModuleArgumentDescription('text', 'Text to apply cipher to', True)]
+    oracle = None
+    oracleRequired = False
+
+    def execute(self):
+        key = self.get_argument_value('key')
+        text = self.get_argument_value('text')
+        xorcipher = RepeatedKeyXOR(key)
+        returntext = xorcipher.crypt(text)
+        print(f'Crypted value: {returntext}')
+        return returntext
