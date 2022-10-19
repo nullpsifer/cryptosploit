1c2d09d09701991262a1443a6c09bdea713d6c12
Matt Cheung <mgcheung@gmail.com>
2022-10-19 14:24:22 -0400

    Some RSA tools for textbook RSA

--- utils/rsautils.py
+++ utils/rsautils.py
@@ -1,5 +1,15 @@
 import os
-from gmpy2 import gcd
+from gmpy2 import gcd, iroot
+from utils.math import sunzi
+
+
+class BroadcastException(Exception):
+    def __init__(self, number, power):
+        self.number = number
+        self.power = power
+
+    def __str__(self):
+        return f'{self.number} is not a perfect {self.power}'
 
 def factor_with_exponents(e,d,n):
     k=e*d-1
@@ -24,3 +34,16 @@ def factor_with_exponents(e,d,n):
                     break
                 q = n//p
                 return p,q
+
+def broadcast_small_e(messagesandkeys, publicexponent):
+    commonexponentlist = [message for message in messagesandkeys if message['e'] == publicexponent]
+    assert len(commonexponentlist) >= publicexponent
+    commonexponentlist.sort(key=lambda x:x['n'])
+    commonexponentlist = commonexponentlist[:publicexponent]
+    congruences = [(message['c'],message['n']) for message in commonexponentlist]
+    largec, _ = sunzi(congruences)
+    m,diditwork = iroot(largec, publicexponent)
+    if diditwork:
+        return int(m)
+    else:
+        raise BroadcastException(largec,publicexponent)
